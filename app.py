f2557d483acf4602d396c9a0b694ecbd086edc34
Paolo Stet <paolostet@gmail.com>
2023-07-01 21:45:48 +0200

    Add Wake on lan

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from wakeonlan import send_magic_packet
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/hello", methods=["POST"])
+@app.route("/wol", methods=["POST"])
 def hello():
     data = request.json
+    mac_address = data.get("mac-address")
     print(data)
-    return jsonify({"message": "Hello, world!"})
+    send_magic_packet(mac_address)
+
+    return jsonify({"message": "Magic packet sent"})
 
 
 if __name__ == "__main__":
